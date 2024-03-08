004aa9e7d37c931e9125b71c13344942be8a4180
Thiago Hoff <thiagohoffsantos@gmail.com>
2024-03-08 15:47:41 -0300

    feat(dynamiccontent): adicionar arquivo que irá gerar informações para o app

---
+++ src/utils/dynamicContent.ts
@@ -0,0 +1,26 @@
+import { House } from "@phosphor-icons/react/dist/ssr";
+
+export const MenuItems = [
+  {
+    id: 1,
+    label: "Home",
+    href: "#",
+    icon: {
+      IconComponente: House,
+      width: 30,
+      height: 30,
+      color: "#fff",
+    },
+  },
+  {
+    id: 2,
+    label: "Lançamentos",
+    href: "#",
+    icon: {
+      IconComponente: House,
+      width: 30,
+      height: 30,
+      color: "#fff",
+    },
+  },
+];
